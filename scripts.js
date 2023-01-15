1978e8f6731222e8bea2ea5f3af3218391e4b36b
LuccasVieira <luccas_vieira3@hotmail.com>
2023-01-15 09:24:24 -0300

    Aula1

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("referencia").innerHTML="<h1>Hello world</h1>";//Injeta codigo no Index.html
+console.log("Hello world");//Imprime no console do inspecionar elemento
+//alert("Ola mundo"); // Faz aparecer um popup na tela
